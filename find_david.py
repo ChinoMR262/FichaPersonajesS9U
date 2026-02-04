b5d9e3cb0832a11d00a7d50f5b368ab4ce678dbf
Chino <chinooficial262@gmail.com>
2026-02-03 22:05:25 -0300

    Delete find_david.py
    Signed-off-by: Chino <chinooficial262@gmail.com>

--- find_david.py
+++
@@ -1,5 +0,0 @@
-
-with open('c:/Users/Chino/Desktop/files/S9U Helios Engine Data v8.html', 'r', encoding='utf-8') as f:
-    for i, line in enumerate(f):
-        if 'David' in line or 'david' in line:
-            print(f"Line {i+1}: {line.strip()[:200]}")
